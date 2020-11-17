4ccf159d5f09852e7a43c823f709ff7b461757c6
Alexandre Vasconcelos <vasconcelosar9@gmail.com>
2020-11-17 19:45:03 -0300

    Mensagem de bom dia acrescentada

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,6 +7,8 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+            Console.ReadLine();
         }
     }
 }
